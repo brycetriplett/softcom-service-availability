4bf818d67efc2d43f6a6760812178004e102e6c3
Bryce Triplett <brycetriplett@gmail.com>
2023-10-02 13:48:38 -0700

    added link to avatar

--- src/components/BaseTemplate.jsx
+++ src/components/BaseTemplate.jsx
@@ -1,42 +1,43 @@
-import React from 'react'
-import { Typography, Box, Container, Avatar } from '@mui/material'
-import MainStepper from './MainStepper'
-import MainFooter from './MainFooter'
-
+import React from "react";
+import { Typography, Box, Container, Avatar } from "@mui/material";
+import MainStepper from "./MainStepper";
+import MainFooter from "./MainFooter";
 
 const BaseTemplate = ({ children, activeStep }) => {
   return (
     <>
-    <Container component="main" maxWidth="md" sx={{mb:  10}}>
+      <Container component="main" maxWidth="md" sx={{ mb: 10 }}>
         <Box
-            minHeight="100vh"
-            sx={{
+          minHeight="100vh"
+          sx={{
             marginTop: 5,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            }}
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-            <Avatar 
-            alt="Softcom" 
-            src="https://www.softcom.net/wp-content/uploads/2016/07/Softcom_Testimonials_2-300x300.jpg"
-            sx={{
+          <a href="https://www.softcom.net">
+            <Avatar
+              alt="Softcom"
+              src="https://www.softcom.net/wp-content/uploads/2016/07/Softcom_Testimonials_2-300x300.jpg"
+              sx={{
                 width: 75,
                 height: 75,
-            }}
+              }}
             />
+          </a>
 
-            <Typography component="h1" variant="h5" sx={{marginTop: 2}}>
-                Check Availability
-            </Typography>
+          <Typography component="h1" variant="h5" sx={{ marginTop: 2 }}>
+            Check Availability
+          </Typography>
 
-            <MainStepper activeStep={activeStep}/>
-            {children}
+          <MainStepper activeStep={activeStep} />
+          {children}
         </Box>
-    </Container>
-    <MainFooter />
+      </Container>
+      <MainFooter />
     </>
-  )
-}
+  );
+};
 
-export default BaseTemplate
+export default BaseTemplate;
--- src/components/Confirmation/ChosenPrice.jsx
+++ src/components/Confirmation/ChosenPrice.jsx
@@ -1,50 +1,51 @@
-import React from 'react'
-import { Box, Card, CardContent, CardHeader, Typography } from '@mui/material'
-
+import React from "react";
+import { Box, Card, CardContent, CardHeader, Typography } from "@mui/material";
 
 const ChosenPrice = ({ tier }) => {
   return (
-    <Card sx={{
+    <Card
+      sx={{
         width: 275,
-      }}>
-        <CardHeader
-          title={tier.title}
-          titleTypographyProps={{ align: 'center' }}
+      }}
+    >
+      <CardHeader
+        title={tier.title}
+        titleTypographyProps={{ align: "center" }}
+        sx={{
+          backgroundColor: (theme) => theme.palette.grey[300],
+        }}
+      />
+      <CardContent>
+        <Box
           sx={{
-            backgroundColor: (theme) => theme.palette.grey[300]
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "baseline",
+            mb: 2,
           }}
-        />
-        <CardContent>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'baseline',
-              mb: 2,
-            }}
-          >
-            <Typography component="h2" variant="h3" color="text.primary">
-              ${tier.price}
+        >
+          <Typography component="h2" variant="h3" color="text.primary">
+            ${tier.price}
+          </Typography>
+          <Typography variant="h6" color="text.secondary">
+            /mo
+          </Typography>
+        </Box>
+        <ul>
+          {tier.description.map((line) => (
+            <Typography
+              component="li"
+              variant="subtitle1"
+              align="left"
+              key={line}
+            >
+              {line}
             </Typography>
-            <Typography variant="h6" color="text.secondary">
-              /mo
-            </Typography>
-          </Box>
-          <ul>
-            {tier.description.map((line) => (
-              <Typography
-                component="li"
-                variant="subtitle1"
-                align="left"
-                key={line}
-              >
-                {line}
-              </Typography>
-            ))}
-          </ul>
-        </CardContent>
-      </Card>
-  )
-}
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default ChosenPrice
+export default ChosenPrice;
